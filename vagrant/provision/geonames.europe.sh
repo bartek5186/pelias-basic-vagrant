e17c8114cca954d16d5a1f52ecbc64dbf104c9c4
bartek5186 <bartek5186@gmail.com>
2019-03-27 20:09:36 +0100

    Fix bash array

--- vagrant/provision/geonames.europe.sh
+++ vagrant/provision/geonames.europe.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env bash
-
-european_countries = [
- "PL", "DE", "AD","AL","AT","AX","BA","BG","BE","BY","CH",
- "CY","CZ","DK","EE","ES","FI","FO","FR",
- "GB","GG","GI","GR","HR","HU","IE","IM","IS",
- "IT","JE","XK","LI","LT","LU","LV","MC","MD",
- "ME","MK","MT","NL","NO",,"PT","RO","RS",
- "RU","SE","SI","SJ","SK","SM","UA","VA","CS"
-]
-
-european_countries = ["PL", "DE"]
-
-# @todo there should be names from config
-for country in european_countries; do
-  sed -i 's/"countryCode".*/"countryCode": {$country}/g' /home/ubuntu/pelias.json
+#!/bin/bash                                                                     
+         
+european_countries=( "PL" "DE" "AD" "AL" "AT" "AX" "BA" "BG" "BE"
+ "BY" "CH" "CY" "CZ" "DK" "EE" "ES" "FI" "FO" "FR" "GB" 
+ "GG" "GI" "GR" "HR" "HU" "IE" "IM" "IS" 
+ "IT" "JE" "XK" "LI" "LT" "LU" "LV" "MC" "MD" 
+ "ME" "MK" "MT" "NL" "NO" "PT" "RO" "RS" 
+ "RU" "SE" "SI" "SJ" "SK" "SM" "UA" "VA" "CS")
+                                                                                 
+for country in ${european_countries[@]}; do                                     
+ #sed -i 's/"countryCode".*/"countryCode": ${country}/g' /home/ubuntu/pelias.json
+ #npm start                                                                      
+ echo ${country}
 done
